26a7f87e20a3427cbc24ec97171abd1f13145a1f
Bao Nguyen <nqbaojsp@gmail.com>
2026-09-02 22:57:39 +0700

    Update config for appScript

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const cfg = {
   oauthClientSecret: process.env.OAUTH_CLIENT_SECRET || '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',
   oauthRedirectUri: process.env.OAUTH_REDIRECT_URI || 'http://localhost:3000/auth/callback',
   sessionSecret: process.env.SESSION_SECRET || require('crypto').randomBytes(32).toString('hex'),
-  appsScriptUrl: process.env.APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbzU3JlxkBt_kGmyHOn6W9BIap6O6h_E7GwZXyQuFBZlwy2c4wi40--XzUKkaTSD3tyXmg/exec',
+  appsScriptUrl: process.env.APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycby4YRf7PJvy9ZxtlTacteMLSLOgRFu06U6ABWu7gScVaOi1iegjasuAlWHKFVSr6WgBfQ/exec',
   sharedToken: process.env.SHARED_TOKEN || 'dev-shared-token'
 };
 
